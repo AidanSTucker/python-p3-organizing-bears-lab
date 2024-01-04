ac52ec551cf9e598f5cc6183742db70fcb13fc37
elpatron <aidanseantucker@gmail.com>
2024-01-04 13:33:24 -0500

    passing tests

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+    ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+    ('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+    ('Tabitha', 4, 'F', 'Gray', 'Calm', 1),
+    ('Sergeant Brown', 7, 'M', 'Brown', 'Brave', 1),
+    ('Melissa', 6, 'F', 'White', 'Sweet', 1),
+    ('Grinch', 10, 'M', 'Green', 'Grumpy', 1),
+    ('Wendy', 2, 'F', 'Golden', 'Playful', 1),
+    (NULL, NULL, NULL, NULL, NULL, NULL); 
